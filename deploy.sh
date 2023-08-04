d2dfb894f3a38568d3b31e760b67372f45a0799c
zyf <congxiaowojiu6@163.com>
2023-08-04 10:57:02 +0800

    up

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,15 @@
 set -e
 
 
-push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
-
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-cd $dist_path
+cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m "deploy"
+git push -f git@github.com:feifei1995/feifei1995.github.io.git master
 
 cd -
-rm -rf $dist_path
